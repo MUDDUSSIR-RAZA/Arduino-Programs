3f9b36d531cb5e65b0b7edb49eaf1e31d4835f3e
MUDDUSSIR-RAZA <muddussir1247@gmail.com>
2023-04-12 23:07:35 +0500

    Update Arduino.ino

--- Irsensor Flamesensor Buzzer Lasser/Arduino.ino
+++ Irsensor Flamesensor Buzzer Lasser/Arduino.ino
@@ -1,6 +1,6 @@
 // Define the pin numbers for the flame sensor, IR sensor, buzzer, laser, and lights
 #define flamePin 2
-#define irSensorpin 3
+#define ldrSensorpin 3
 #define buzzer 12
 #define laser 11
 #define lights 7
@@ -8,7 +8,7 @@
 void setup() {
   // Set the pin modes for the sensors and output devices
   pinMode(flamePin, INPUT);
-  pinMode(irSensorpin, INPUT);
+  pinMode(ldrSensorpin, INPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(laser, OUTPUT);
 
@@ -18,13 +18,13 @@ void setup() {
 
 void loop() {
   // Read the value of the IR sensor
-  int irSensorValue = digitalRead(irSensorpin);
+  int ldrSensorValue = digitalRead(ldrSensorpin);
 
   // Read the value of the flame sensor
   int flameValue = digitalRead(flamePin);
 
   // If the IR sensor is detecting something, turn on the laser
-  if (irSensorValue == HIGH) {
+  if (ldrSensorValue == HIGH) {
     digitalWrite(laser, HIGH);
   } else {
     digitalWrite(laser, LOW);
@@ -41,7 +41,7 @@ void loop() {
   }
 
   // If both the flame sensor and IR sensor detect something, turn on the lights for 20ms and then turn them off for 20ms
-  if (flameValue == LOW && irSensorValue == HIGH) {
+  if (flameValue == LOW && ldrSensorValue == HIGH) {
     digitalWrite(lights, HIGH);
     delay(20);
     digitalWrite(lights, LOW);
